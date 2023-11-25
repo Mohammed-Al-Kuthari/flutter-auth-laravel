73147dfef9edad3d248571ed2481a4a31553a618
Mohammed Al-Kuthari <alkuthari@hotmail.com>
2023-11-26 00:50:50 +0300

    Create the storage providers.

--- lib/providers/shaired.dart
+++ lib/providers/shaired.dart
@@ -1,6 +1,8 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:learn/controllers/connectivity/connectivity_controller.dart';
+import 'package:learn/infrastructure/credential_storage/secure_token_storage.dart';
 import 'package:learn/services/connectivity/connectivity_service.dart';
 
 final connectivityServiceProvider =
@@ -13,4 +15,12 @@ final connectivityControllerProvider =
 final connectivityStreamProvider = StreamProvider<ConnectivityResult>((ref) {
   final connectivityRepository = ref.read(connectivityServiceProvider);
   return connectivityRepository.listenConnectivity();
+});
+
+final flutterSecureStorageProvider =
+    Provider<FlutterSecureStorage>((ref) => const FlutterSecureStorage());
+
+final secureTokenStorageProvider = Provider<SecureTokenStorage>((ref) {
+  final storage = ref.read(flutterSecureStorageProvider);
+  return SecureTokenStorage(storage);
 });
